0bc4b713d9f96aec890bc85e4959b14e05c58399
Qiu Du <LyleDu89@gmail.com>
2022-04-07 03:31:32 +0800

    Initial Commit

---
+++ Swifty Pusher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swifty Pusher
+//
+//  Created by QIU DU on 7/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swifty Pusher/Swifty_PusherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swifty_PusherApp.swift
+//  Swifty Pusher
+//
+//  Created by QIU DU on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Swifty_PusherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
